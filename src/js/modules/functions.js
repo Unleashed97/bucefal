8fdd4ba0629ec7cd52ddf9f1ef8c7e6da8d854e7
Nichkov Roman <nichkovroman@gmail.com>
2022-12-18 17:41:03 +0300

    feat: modal open

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,1 +1,19 @@
+export const handleModal = () => {
+    const modal = document.querySelector('.modal')
+    const modalOpenBtn = document.querySelector('.modal-open')
+    const modalClose = document.querySelector('.modal-close')
 
+    modalOpenBtn.addEventListener('click', () => {
+        modal.classList.add('active')
+    })
+
+    modalClose.addEventListener('click', () => {
+        modal.classList.remove('active')
+    })
+
+    window.addEventListener('click', ({ target }) => {
+        if (target === modal) {
+            modal.classList.remove('active')
+        }
+    })
+}
